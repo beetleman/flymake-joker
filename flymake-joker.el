1f7e182f746e8b6c24ddb6de7714c364b403d9a8
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2019-11-06 22:31:01 +0100

    simplify code and parse all messages from joker

--- flymake-joker.el
+++ flymake-joker.el
@@ -38,28 +38,52 @@
 (require 'flymake-quickdef)
 
 (setf flymake-check-joker-regexp
-      "^.+:\\([[:digit:]]+\\):\\([[:digit:]]+\\): Parse \\([[:alpha:]]+\\): \\(.+\\)$")
+      "^.+:\\([[:digit:]]+\\):\\([[:digit:]]+\\): \\([[:alpha:]\\ ]+\\): \\(.+\\)$")
 
-(flymake-quickdef-backend flymake-check-joker-clj
-  :pre-let ((joker-exec (executable-find "joker")))
-  :pre-check (unless joker-exec (error "Cannot find joker executable"))
-  :write-type 'pipe
-  :proc-form (list joker-exec "--lint" "-")
-  :search-regexp flymake-check-joker-regexp
-  :prep-diagnostic
-  (let* ((lnum (string-to-number (match-string 1)))
-         (lcol (string-to-number (match-string 2)))
-         (severity (match-string 3))
-         (msg (match-string 4))
-         (pos (flymake-diag-region fmqd-source lnum lcol))
-         (beg (car pos))
-         (end (cdr pos))
-         (type (cond
-                ((string= severity "error") :error)
-                ((string= severity "warning") :warning)
-                ((string= severity "Exception") :error)
-                (t :note))))
-    (list fmqd-source beg end type msg)))
+(defun severity-to-type (severity)
+  (cond
+   ((string= severity "Read error") :error)
+
+   ((string= severity "Parse warning") :warning)
+   ((string= severity "Parse error") :error)
+
+   ((string= severity "Exception") :error)
+
+   (t :note)))
+
+(defmacro defn-joker-backend (name dialect)
+  `(flymake-quickdef-backend ,name
+     :pre-let ((joker-exec (executable-find "joker")))
+     :pre-check (unless joker-exec (error "Cannot find joker executable"))
+     :write-type 'pipe
+     :proc-form (list joker-exec "--lint" "--dialect" ,dialect "-")
+     :search-regexp flymake-check-joker-regexp
+     :prep-diagnostic
+     (let* ((lnum (string-to-number (match-string 1)))
+            (lcol (string-to-number (match-string 2)))
+            (severity (match-string 3))
+            (msg (match-string 4))
+            (pos (flymake-diag-region fmqd-source lnum lcol))
+            (beg (car pos))
+            (end (cdr pos))
+            (type (severity-to-type severity)))
+       (list fmqd-source beg end type msg))))
+
+(defn-joker-backend flymake-check-joker-clj "clj")
+(defn-joker-backend flymake-check-joker-edn "edn")
+(defn-joker-backend flymake-check-joker-cljs "cljs")
+
+(defun setup-flymake-backend-clj ()
+  (let ((ext (file-name-extension (buffer-file-name))))
+    (when (not (string= "cljs" ext))
+      (flymake-mode 1)
+      (if (string= "edn" ext)
+          (add-hook 'flymake-diagnostic-functions 'flymake-check-joker-edn nil t)
+        (add-hook 'flymake-diagnostic-functions 'flymake-check-joker-clj nil t)))))
+
+(defun setup-flymake-backend-cljs ()
+  (flymake-mode 1)
+  (add-hook 'flymake-diagnostic-functions 'flymake-check-joker-cljs nil t))
 
 (provide 'flymake-joker)
 
